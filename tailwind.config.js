9f00e7a80c3b5d662a1ddb6372a06c1e204d208c
ChristL <hcmetuetso@gmail.com>
2022-03-09 13:36:21 -0500

    test-christ

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //Adding the paths to all of the files --content: ["./src/**/*.{html,js}"]
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  //adding daisyUI
+  plugins: [require("daisyui")],
+}
