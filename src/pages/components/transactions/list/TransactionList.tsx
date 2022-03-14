29963be786d017357a3bc7c5fd55d880960fcfb9
JudeSeide <jude.seide@ssense.com>
2022-03-14 15:07:31 -0400

    styling transaction list

--- src/pages/components/transactions/list/TransactionList.tsx
+++ src/pages/components/transactions/list/TransactionList.tsx
@@ -1,33 +1,50 @@
-import ImageIcon from '@mui/icons-material/Image';
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { capitalize, lowerCase } from 'lodash';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 import { Transaction } from '../../../../../types/@finerd';
 
 // todo: transform to a container with cards
 // todo: adjust layout
 export const TransactionList = ({ transactions }: { transactions: Transaction[] }) => (
-    <List sx={{ width: '100%' }}>
+    <Grid container alignItems="center" justifyContent="space-between" sx={{ rowGap: 2, px: 1 }}>
         {
             transactions?.map((transaction) => (
-                <ListItem alignItems="flex-start" sx={{ justifyContent: 'space-between' }} key={transaction.id}>
-                    <ListItemAvatar>
-                        <Avatar>
-                            <ImageIcon/>
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={transaction.category}
-                        secondary={transaction.paymentMethod}
-                    />
-                    <ListItemText
-                        primary={new Intl.NumberFormat('en-CA', {
-                            style: 'currency',
-                            currency: 'CAD'
-                        }).format(transaction.amount)}
-                        secondary={new Intl.DateTimeFormat('en-CA').format(transaction.createdAt)}
-                    />
-                </ListItem>
+                <Grid item xs={12} md={6} key={`${transaction.type}-transaction-${transaction.id}`}>
+                    <CardActionArea>
+                        <Card variant="outlined" sx={{ display: 'flex', flexDirection: 'row' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: '20%', height: '20%', mt: 1.3, ml: 1 }}
+                                image={`/images/${transaction.category}.svg`}
+                                alt={transaction.category}
+                            />
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        {capitalize(transaction.category)}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        {lowerCase(transaction.description)}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', mr: 0 }}>
+                                <CardContent sx={{ textAlign: 'right' }}>
+                                    <Typography component="div" variant="h5" >
+                                        {new Intl.NumberFormat('en-CA', {
+                                            style: 'currency',
+                                            currency: 'CAD'
+                                        }).format(transaction.amount)}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        {new Intl.DateTimeFormat('en-CA').format(transaction.createdAt)}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    </CardActionArea>
+                </Grid>
             ))
         }
-    </List>
+    </Grid>
 );
