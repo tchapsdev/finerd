6a2e89988218029a7cdf3ce2e86a098e69e3d543
JudeSeide <jude.seide@ssense.com>
2022-03-15 00:18:03 -0400

    fix lints

--- src/components/transactions/transactionInfo/TransactionInfo.tsx
+++ src/components/transactions/transactionInfo/TransactionInfo.tsx
@@ -1,81 +1,63 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import { Avatar, Box, Button, Card, CardContent, Grid, Paper, Stack, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 
-
-
-
-
-export const TransactionInfo = () => transactionInfo() ; 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
-let transaction = { "Name":"Uber Eats","Date":"2022-03-14","Category":"Food","Amount":19.99,"Payementmethod":"Card" }; 
+	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	color: theme.palette.text.secondary,
+	padding: theme.spacing(1),
+	textAlign: 'center',
+	...theme.typography.body2,
+}));
 
-export function transactionInfo() {
+let transaction = {
+	Amount: 19.99,
+	Category: 'Food',
+	Date: '2022-03-14',
+	Name: 'Uber Eats',
+	Payementmethod: 'Card',
+};
 
-  return (
-    
-    <Box sx={{ width: "100%"}}>
-      <Stack spacing={2}>
-            <Card sx={{ display: 'flex',  justifyContent:"space-around"}}>
-                <Box sx={{  flexDirection: 'column'}}>
-                    <CardContent>
-                    <Typography component="div" variant="h5">
-                        {transaction["Name"]}
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                        {transaction["Date"]}
-                    </Typography>
-                    </CardContent>
-                </Box>
-                <Avatar
-                alt="Dépense"
-                src="/public/icons/icon-96x96.png"
-                sx={{ width: 90, height: 90  }}
-                />
-            </Card>
-          
-            <Stack  spacing={2} sx={{  flexDirection: 'column'}}>
-                <h1> Category : {transaction["Category"]}  </h1>
-                <h4> Amount :  {transaction["Amount"]}</h4>
-                <h1> Payement Method : {transaction["Payementmethod"]} </h1>
-            </Stack>
-        </Stack> 
-
-        <Box sx={{ width: '100%' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                <Item> <Button size="medium"  variant="contained" color="error" >Delete</Button> </Item>
-                </Grid>
-                <Grid item xs={6}>
-                <Item> <Button size="medium"  variant="contained" color="success" >Edit</Button> </Item>
-                </Grid>
-            </Grid>
-        </Box>
-
-    </Box>
-
-    
-
-
-        
-
-
-// image={require("/public/icons/icon-16x16.png")} // require image
-
-    
-  );
-}
+export const TransactionInfo = () => (
+	<Box sx={{ width: '100%' }}>
+		<Stack spacing={2}>
+			<Card sx={{ display: 'flex', justifyContent: 'space-around' }}>
+				<Box sx={{ flexDirection: 'column' }}>
+					<CardContent>
+						<Typography component="div" variant="h5">
+							{transaction['Name']}
+						</Typography>
+						<Typography variant="subtitle1" color="text.secondary" component="div">
+							{transaction['Date']}
+						</Typography>
+					</CardContent>
+				</Box>
+				<Avatar alt="Dépense" src="/public/icons/icon-96x96.png" sx={{ height: 90, width: 90 }} />
+			</Card>
+			<Stack spacing={2} sx={{ flexDirection: 'column' }}>
+				<h1> Category : {transaction['Category']} </h1>
+				<h4> Amount : {transaction['Amount']}</h4>
+				<h1> Payement Method : {transaction['Payementmethod']} </h1>
+			</Stack>
+		</Stack>
+		<Box sx={{ width: '100%' }}>
+			<Grid container rowSpacing={1} columnSpacing={{ md: 3, sm: 2, xs: 1 }}>
+				<Grid item xs={6}>
+					<Item>
+						{' '}
+						<Button size="medium" variant="contained" color="error">
+							Delete
+						</Button>{' '}
+					</Item>
+				</Grid>
+				<Grid item xs={6}>
+					<Item>
+						{' '}
+						<Button size="medium" variant="contained" color="success">
+							Edit
+						</Button>{' '}
+					</Item>
+				</Grid>
+			</Grid>
+		</Box>
+	</Box>
+);
