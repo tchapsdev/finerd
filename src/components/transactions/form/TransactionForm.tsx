0e22f5855d1ab4d9a5e9d096913989d068dcf274
JudeSeide <jude.seide@ssense.com>
2022-03-15 23:22:18 -0400

    add icons

--- src/components/transactions/form/TransactionForm.tsx
+++ src/components/transactions/form/TransactionForm.tsx
@@ -86,7 +86,7 @@ export const TransactionForm = ({ isLoading }: { isLoading: boolean }) => {
 	};
 
 	const transactionType = supportedTransactions[currentPanel];
-	const transaction: Transaction = currentTransaction || { id: 0, type: transactionType, amount: 0 };
+	const transaction: Transaction = currentTransaction || { amount: 0, id: 0, type: transactionType };
 
 	const transactionService = new TransactionService();
 	const imageInputRef = useRef<HTMLInputElement>(null);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ export const App = ({ Component, pageProps }: AppProps) => (
 			<link rel="manifest" href="/manifest.json" />
 			<link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
 			<link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-			<link rel="apple-touch-icon" href="/apple-icon.png"></link>
+			<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
 			<meta name="theme-color" content="#317EFB" />
 		</Head>
 		<Component {...pageProps} />
